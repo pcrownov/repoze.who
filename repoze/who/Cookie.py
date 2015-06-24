44cb1e0e6eda4215a8ad043be95072ae9f884a72
Patrick Crownover <pcrownov@redhat.com>
2015-06-24 13:41:42 -0400

    syntax error

--- repoze/who/Cookie.py
+++ repoze/who/Cookie.py
@@ -664,7 +664,17 @@ class BaseCookie(dict):
     # end __ParseString
 # end BaseCookie class
 
-[docs]class SimpleCookie(BaseCookie): """SimpleCookie SimpleCookie supports strings as cookie values. When setting the value using the dictionary assignment notation, SimpleCookie calls the builtin str() to convert the value to a string. Values received from HTTP are kept as strings. """ def value_decode(self, val): return _unquote( val ), val def value_encode(self, val): strval = str(val) return strval, _quote( strval ) # end SimpleCookie
+class SimpleCookie(BaseCookie): 
+    """SimpleCookie SimpleCookie supports strings as cookie values. When setting 
+    the value using the dictionary assignment notation, SimpleCookie 
+    calls the builtin str() to convert the value to a string. Values
+    received from HTTP are kept as strings. """ 
+    def value_decode(self, val): 
+        return _unquote( val ), val 
+    def value_encode(self, val): 
+        strval = str(val) 
+        return strval, _quote( strval ) 
+# end SimpleCookie
 
 class SerialCookie(BaseCookie):
     """SerialCookie
